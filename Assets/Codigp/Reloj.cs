ea908287a3f28fe33611de6e3185abd09969a99c
Daihan <60204121+Johan690@users.noreply.github.com>
2025-10-28 19:30:08 -0600

    time

--- Assets/Codigp/Reloj.cs
+++ Assets/Codigp/Reloj.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
+using System;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Reloj : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] int min, seg;
+    [SerializeField] Text tiempo;
+
+    private float restante;
+    private bool enMarcha;
+
+    public GameObject Canlas;
+
+    private void Awake()
     {
-        
+        restante = (min * 60) + seg;
+        enMarcha = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (enMarcha)
+        {
+            restante -= Time.deltaTime;
+            if (restante < 1)
+            {
+                Canlas.SetActive(true);
+                enMarcha = true;
+            }
+            int tempMin = Mathf.FloorToInt(restante / 60);
+            int tempSeg = Mathf.FloorToInt(restante % 60);
+            tiempo.text = string.Format("{00:00}:{01:00}", tempMin, tempSeg);
+        }
     }
 }
